6d11d07832cb7302247d7b8a12aab67b42fcf42b
mal-key <malulu620@gmail.com>
2024-11-07 00:44:26 +0900

    タイムライン取得機能の修正

--- API/views.py
+++ API/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
 from API.models import Users, Questions, Answers, Favorites
 from rest_framework import status
 from rest_framework.views import APIView
@@ -10,10 +8,7 @@ from API.serializers import QuestionsSerializer
 #タイムライン記事リスト取得
 class TimelineView(APIView):
     def get(self, request, *args, **kwargs):
-        instance = Questions.objects.all()
+        instance = Questions.objects.select_related('user').all()
         serializer = QuestionsSerializer(instance, many=True)
         resp = {"questions": serializer.data}
         return Response(resp, status.HTTP_200_OK)
-    
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
