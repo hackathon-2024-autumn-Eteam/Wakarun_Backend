f7507cdc071f1af4101dcedfe3497fb1eb893caf
mal-key <malulu620@gmail.com>
2024-11-27 23:05:38 +0900

    signin関係エンドポイント名修正＆migrate

--- API/urls.py
+++ API/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     path('timeline/', views.TimelineView.as_view()),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('signin/', TokenObtainPairView.as_view(), name='signin'),
+    path('signin/refresh/', TokenRefreshView.as_view(), name='signin_refresh'),
     path('signup/', views.RegisterView.as_view(), name='signup'),
     path('questions/', views.CreateQuestionView.as_view(), name='questions'),
     path('answers/', views.CreateAnswersView.as_view(), name='questions'),
